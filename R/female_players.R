6e97e6ec1f52ccc7917bd377e1b409485b39ba74
RobWHickman <robwhickman@gmail.com>
2020-04-05 14:10:35 +0100

    added scripts to load data

---
+++ R/female_players.R
@@ -0,0 +1,9 @@
+#' Data on Female Players in the FIFA Video Game Dataset 2016-2019
+#'
+#' All female characters in the game set with most
+#' (maybe not all?) stats and traits for those players
+#'
+#' @format A data frame with 1536 rows and 62 variables:
+#' \describe{
+#' }
+"female_players"
---
+++ R/male_players.R
@@ -0,0 +1,9 @@
+#' Data on Male Players in the FIFA Video Game Dataset 2005-2019
+#'
+#' Most (not all yet) male characters in the game set with most
+#' (maybe not all?) stats and traits for those players
+#'
+#' @format A data frame with 141504 rows and 62 variables:
+#' \describe{
+#' }
+"male_players"
